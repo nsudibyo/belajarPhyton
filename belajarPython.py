96f4c779c0aacbc5985f3a96e3957f9070f4454c
Nurhadi Sudibyo <149948364+nsudibyo@users.noreply.github.com>
2023-11-25 08:01:00 +0700

    Update belajarPython.py

--- belajarPython.py
+++ belajarPython.py
@@ -1,2 +1,8 @@
-name = input('Masukkan nama Anda:')
-print(name)
+print("Selamat datang dalam program Python!\n")
+print("Silakan masukkan data diri Anda.")
+nama = input("Masukkan nama Anda: ")
+tahun_lahir = input("Masukkan tahun lahir Anda: ")
+umur = 2023 - int(tahun_lahir)
+ 
+print(f"Selamat datang {nama} dalam program Python, per 2023 umur Anda adalah {umur} tahun.\n")
+print("Terima kasih telah menggunakan program Python!")
